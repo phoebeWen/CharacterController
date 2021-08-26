9283e66d7a2137c2f43b2214ace8ee6b33f86841
wen <xuemei.wen@gaea.com>
2021-08-26 17:37:07 +0800

    update

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    // public Button inventoryBtn;
+
+    void Start()
+    {
+        // inventoryBtn.onClick.AddListener(OnClickInventory);
+    }
+    
+    public void OnClickInventory()
+    {
+        Debug.Log("abcd");
+    }
+}
