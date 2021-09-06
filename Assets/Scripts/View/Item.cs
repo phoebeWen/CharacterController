9574b53009f58a7a1b539ed0d251264bbb09fdf9
wen <xuemei.wen@gaea.com>
2021-09-06 19:18:32 +0800

    update

---
+++ Assets/Scripts/View/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image icon;
+    public Transform itemParent;
+
+    void Start()
+    {
+        // 暂时
+        itemParent = transform.parent;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.SetParent(transform.parent.parent);
+        transform.position = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(itemParent);
+    }
+}
