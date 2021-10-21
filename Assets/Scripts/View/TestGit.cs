9f62e36bf1bd2171aca70df602881a1f6bc25239
wen <xuemei.wen@gaea.com>
2021-10-21 16:02:17 +0800

    test git

--- Assets/Scripts/View/TestGit.cs
+++ Assets/Scripts/View/TestGit.cs
@@ -7,7 +7,7 @@ public class TestGit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test Git");
     }
 
     // Update is called once per frame
